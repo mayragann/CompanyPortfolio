d59e04709d50c5429803771d5450427dead9be8f
mayragann <mayragann@gmail.com>
2022-07-07 08:12:34 -0400

    Added Homepage Main section + Started Homepage Styling

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Components/Home/Home";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   );
 }
