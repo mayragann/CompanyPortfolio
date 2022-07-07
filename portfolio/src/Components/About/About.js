84a522d9c2f1961dfd93b93e13849968e7da85b1
mayragann <mayragann@gmail.com>
2022-07-07 09:26:37 -0400

    Added gird to About Section and Stylized it

--- portfolio/src/Components/About/About.js
+++ portfolio/src/Components/About/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./About.css";
 import about from "../../Images/About.jpg";
+import { FcSlrBackSide, FcWorkflow, FcShop } from "react-icons/fc";
 
 const About = () => {
   return (
@@ -11,16 +12,64 @@ const About = () => {
             <img src={about} height="375px" width="375px" alt="about_image" />
           </div>
         </div>
-        <div classNdfgame="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+        <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12">
           <div className="about-details">
             <div className="about-title">
               <h5>About</h5>
               <span className="line"></span>
             </div>
+
             <p>
               This is a very interesting location to add information about your
               company
             </p>
+
+            <div className="card-design">
+              <div className="about-list-item">
+                <div className="about-details-icon">
+                  <p><FcSlrBackSide size={50}/></p>
+                </div>
+
+                <div>
+                  <h6>Perfect Day Out</h6>
+                  <p>
+                    This is a great idea for you to try for your next big job.
+                    See how much we can accomplish
+                  </p>
+                </div>
+                </div>
+            </div>
+            <div className="card-design">
+              <div className="about-list-item">
+                <div className="about-details-icon">
+                  <p><FcWorkflow size={50}/></p>
+                </div>
+
+                <div>
+                  <h6>Perfect Day Out</h6>
+                  <p>
+                    This is a great idea for you to try for your next big job.
+                    See how much we can accomplish
+                  </p>
+                </div>
+                </div>
+            </div>
+            <div className="card-design">
+              <div className="about-list-item">
+                <div className="about-details-icon">
+                  <p><FcShop size={50}/></p>
+                </div>
+
+                <div>
+                  <h6>Perfect Day Out</h6>
+                  <p>
+                    This is a great idea for you to try for your next big job.
+                    See how much we can accomplish
+                  </p>
+                </div>
+                </div>
+            </div>
+
           </div>
         </div>
       </div>
