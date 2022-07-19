81cfa3407ceabb059c77b2179e9b0788330aff18
mayragann <mayragann@gmail.com>
2022-07-19 11:05:40 -0400

    Added Further Stylization of cards for Career

--- portfolio/src/Components/Career/DataInfo.js
+++ portfolio/src/Components/Career/DataInfo.js
@@ -1,17 +1,21 @@
-import React from 'react'
-import { FcCollapse,FcExpand } from 'react-icons/fc';
+import React, { useState } from "react";
+import { FcCollapse, FcExpand } from "react-icons/fc";
 
-const DataInfo = ({title,body}) => {
+const DataInfo = ({ title, body }) => {
+  const [show, setShow] = useState(false);
+
   return (
-    <div className="post-card">
-        <div className="collapse-option">
-            <h6>{title}</h6>
-            <p><FcCollapse size={20}/></p>
-        </div>
-        <p>{body}</p>
-
+    <div
+      className={show ? "post-card-open" : "post-card"}
+      onClick={() => setShow(!show)}
+    >
+      <div className="collapse-option">
+        <h6>{title}</h6>
+        <p>{show ? <FcCollapse size={20} /> : <FcExpand size={20} />}</p>
+      </div>
+      <div className="description">{show ? <p>{body}</p> : null}</div>
     </div>
-  )
-}
+  );
+};
 
 export default DataInfo;
