d5dcd11a81a846bcb267370dd5ca9a31e9e5fe86
mayragann <mayragann@gmail.com>
2022-07-19 09:01:10 -0400

    Linked Video to Navbar + Further Styling of Video Car

--- portfolio/src/Components/Navbar/Navbar.js
+++ portfolio/src/Components/Navbar/Navbar.js
@@ -1,75 +1,104 @@
 import React, { useState } from "react";
-import './Navbar.css';
-import { Link } from 'react-scroll';
+import "./Navbar.css";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
+  const [navScrollColor, setNavScrollColor] = useState(false);
+  const onChangeNavColor = () => {
+    if (window.scrollY >= 100) {
+      setNavScrollColor(true);
+    } else {
+      setNavScrollColor(false);
+    }
+  };
+  window.addEventListener("scroll", onChangeNavColor);
 
-    const [navScrollColor, setNavScrollColor] = useState (false);
-    const onChangeNavColor = () => {
-        if (window.scrollY >= 100) {
-            setNavScrollColor (true);
-        }
-        else{
-            setNavScrollColor(false);
-        }
-    };
-    window.addEventListener("scroll", onChangeNavColor);
+  return (
+    <nav
+      className={
+        navScrollColor ? "navbar-main navbar-scroll-color" : "navbar-main"
+      }
+    >
+      <ul>
+        <li className="nav-item">
+          <Link to="home" spy={true} smooth={true} offset={-100} duration={100}>
+            Home
+          </Link>
+        </li>
 
-    return (
-        <nav className={navScrollColor ? "navbar-main navbar-scroll-color":"navbar-main"}>
-            <ul>
-                <li className="nav-item">
-                <Link to="home" spy={true} smooth={true} offset={-100} duration={100} >
-                    Home
-                </Link>
-                </li>
+        <li className="nav-item">
+          <Link to="about" spy={true} smooth={true} offset={-70} duration={100}>
+            About
+          </Link>
+        </li>
 
-                <li className="nav-item">
-                <Link to="about" spy={true} smooth={true} offset={-70} duration={100}>
-                    About
-                </Link>
-                </li>
+        <li className="nav-item">
+          <Link
+            to="service"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={100}
+          >
+            Services
+          </Link>
+        </li>
+        <li className="nav-item">Career</li>
+        <li className="nav-item">
+          <Link
+            to="video"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={100}
+          >
+            Video
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="team" spy={true} smooth={true} offset={-100} duration={100}>
+            Team
+          </Link>
+        </li>
 
-                <li className="nav-item">
-                <Link to="service" spy={true} smooth={true} offset={-100} duration={100}>
-                    Services
-                </Link>
-                </li>
-                <li className="nav-item">
-                    Career
-                </li>
-                <li className="nav-item">
-                    Video
-                </li>
-                <li className="nav-item">
-                <Link to="team" spy={true} smooth={true} offset={-100} duration={100}>
-                    Team
-                </Link>
-                </li>
+        <li className="nav-item">
+          <Link
+            to="testimonials"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={100}
+          >
+            Testimonials
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="feedback"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={100}
+          >
+            Feedback
+          </Link>
+        </li>
+        <li className="nav-item">Contact</li>
 
-                <li className="nav-item">
-                <Link to="testimonials" spy={true} smooth={true} offset={-100} duration={100}>
-                    Testimonials
-                </Link>
-                </li>
-                <li className="nav-item">
-                <Link to="feedback" spy={true} smooth={true} offset={-100} duration={100}>
-                    Feedback
-                </Link>
-                </li>
-                <li className="nav-item">
-                    Contact
-                </li>
-
-                <li className="nav-item">
-                <Link to="subscribe" spy={true} smooth={true} offset={-100} duration={100}>
-                    Subscribe
-                </Link>
-                </li>
-            </ul>
-
-        </nav>
-    );
-}
+        <li className="nav-item">
+          <Link
+            to="subscribe"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={100}
+          >
+            Subscribe
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
--- portfolio/src/Components/Video/Video.js
+++ portfolio/src/Components/Video/Video.js
@@ -1,67 +1,79 @@
-import React from 'react'
-import "./Video.css"
-import ReactPlayer from 'react-player'
+import React from "react";
+import "./Video.css";
+import ReactPlayer from "react-player";
 
 const Video = () => {
-
-    const data =[
-        {
-            id:1,
-            url:"https://youtu.be/nTf2p36NAi8",
-            title:"Speed Run of our Coding in process"
-        },
-        {
-            id:2,
-            url:"https://youtu.be/SRec90j6lTY",
-            title:"Explanation of design"
-        },
-        {
-            id:3,
-            url:"https://youtu.be/8Sj2tbh-ozE",
-            title:"Marketing 101"
-        },
-        {
-            id:4,
-            url:"https://youtu.be/dFSia1LZI4Y",
-            title:"Graphic design"
-        },
-        {
-            id:5,
-            url:"https://youtu.be/JA9BqmqqhsI",
-            title:"Design trends of the year"
-        },
-        {
-            id:6,
-            url:"https://youtu.be/_2LLXnUdUIc",
-            title:"An explanation on color theory"
-        }
-
-    ]
+  const data = [
+    {
+      id: 1,
+      url: "https://youtu.be/nTf2p36NAi8",
+      title: "Speed Run of Coding in process",
+      content:
+        "Check out this pretty cool video of a screen of a coding professional.",
+    },
+    {
+      id: 2,
+      url: "https://youtu.be/SRec90j6lTY",
+      title: "Explanation of design",
+      content:
+        "This is all very helpful information to learn more about designing.",
+    },
+    {
+      id: 3,
+      url: "https://youtu.be/8Sj2tbh-ozE",
+      title: "Marketing 101",
+      content:
+        "Very helpful information on a little more information about marketing.",
+    },
+    {
+      id: 4,
+      url: "https://youtu.be/dFSia1LZI4Y",
+      title: "Graphic design",
+      content: "Graphic design is a very helpful tool.",
+    },
+    {
+      id: 5,
+      url: "https://youtu.be/JA9BqmqqhsI",
+      title: "Design trends of the year",
+      content: "These are some pretty nifty designs.",
+    },
+    {
+      id: 6,
+      url: "https://youtu.be/_2LLXnUdUIc",
+      title: "An explanation on color theory",
+      content:
+        "Very useful information for anyone that wants to learn more about color theory.",
+    },
+  ];
   return (
     <div className="container video">
-        <div className="section-title">
-            <h5>Video</h5>
-            <span className="line"></span>
-        </div>
-        
-        <div className="row">
-            {data.map((item,index) => (
-                <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12">
-                    <div className="video-details">
-                    <ReactPlayer url={item.url} controls width="auto" height="300px"/>
+      <div className="section-title">
+        <h5>Video</h5>
+        <span className="line"></span>
+      </div>
 
-                    </div>
-
-
-                </div>
-
-            ))}
-
-
-        </div>
-
+      <div className="row" >
+        {data.map((item, index) => (
+          <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12" key={index} id="video">
+            <div className="video-details">
+              <ReactPlayer
+                url={item.url}
+                loop={true}
+                playing={false}
+                controls
+                width="auto"
+                height="250px"
+              />
+              <div className="video-content">
+              <h6>{item.title}</h6>
+              <p>{item.content}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Video
+export default Video;
